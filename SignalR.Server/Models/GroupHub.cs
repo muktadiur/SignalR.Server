9a873720514394074a3ca3508fae227c77d53a2e
muktadiur <muktadiur@gmail.com>
2013-10-04 22:11:30 +0600

    initial Commit

---
+++ SignalR.Server/Models/GroupHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace IMS.SignalR.Models
+{
+    
+    public class GroupHub : Hub
+    {
+        public void Send(string name, string msg) {
+            Clients.Group(name).Show(name, msg);
+        }
+        
+        public void Join(string name) {
+            Groups.Add(Context.ConnectionId, name);
+        }
+        public void Remove(string name) {
+            Groups.Remove(Context.ConnectionId, name);
+        }
+    }
+}
